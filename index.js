e58f1adc51d05a399e647e0a887d68d58752df7d
Taras Burdeniuk <bur.taras@gmail.com>
2019-03-12 12:19:36 +0200

    edited index.js

--- index.js
+++ index.js
@@ -2,28 +2,16 @@ const TelegramBot = require('node-telegram-bot-api');
 const http = require('http');
 const https = require('https');
 
-http.createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
-    res.end('')
-});
-setInterval(function(){
-    https.get('https://telegrambotwhichshowsome.herokuapp.com/')
-},300000);
+// http.createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+//     res.end('')
+// });
+// setInterval(function(){
+//     https.get('https://telegrambotwhichshowsome.herokuapp.com/')
+// },300000);
 
 
 const token = '779165592:AAGwcIPKr4qua2WHW7LnPl8pvAH24RB34ps';
 const bot = new TelegramBot(token, {polling: true});
-
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//     // 'msg' is the received Message from Telegram
-//     // 'match' is the result of executing the regexp above on the text content
-//     // of the message
-//
-//     const chatId = msg.chat.id;
-//     const resp = match[1]; // the captured "whatever"
-//
-//     // send back the matched "whatever" to the chat
-//     bot.sendMessage(chatId, resp);
-// });
 
 // Listen for any kind of message. There are different kinds of
 // messages.
@@ -33,7 +21,6 @@ bot.sendMessage(630358462, `Bot is running`);
 const deadline = 1552417200;
 
 bot.on('message', (msg) => {
-    // if (msg.text === '/deadline') {
     const chatId = msg.chat.id;
     const date = new Date(msg.date * 1000);
     console.log(msg);
@@ -57,11 +44,4 @@ bot.on('message', (msg) => {
         default:
             bot.sendMessage(chatId, 'hello');
     }
-
-
-    // } else {
-    //     const chatId = msg.chat.id;
-
-    // bot.sendMessage(chatId, `Hello`);
-    // }
 });
