e1596bce4627cf22d2da888c50ecfa22a5881c1c
Taras Burdeniuk <bur.taras@gmail.com>
2019-03-12 12:25:08 +0200

    edited index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const http = require('http');
 const https = require('https');
 
-// http.createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
-//     res.end('')
-// });
+http.createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+});
 // setInterval(function(){
 //     https.get('https://telegrambotwhichshowsome.herokuapp.com/')
 // },300000);
